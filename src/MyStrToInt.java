ee8414be3620f443aeaacc8a0de044fd8978cc68
Nakarin Suppakun <nakarin_sup@hotmail.com>
2021-08-19 23:16:25 +0700

    first version of str2int,  not support the negative value (all minus signs are discarded).

---
+++ src/MyStrToInt.java
@@ -0,0 +1,98 @@
+
+/**
+ * This has function to convert from string that contains mix characters into integer<br>
+ * 
+ * The function always return positive integer, all minus sign in string would be discard.<br>
+ *
+ * @author nakarin_sup
+ *
+ */
+public class MyStrToInt {
+
+	/** with this char array, we can map with the corresponding index directly. */
+	private final char[] charNumberList = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+	/**
+	 * map character to digit as integer
+	 * 
+	 * @param character
+	 * @return digit number
+	 */
+	protected int getNumber(char character) {
+
+		for (int i = 0; i < charNumberList.length; ++i) {
+			if (character == charNumberList[i]) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	/**
+	 * a^b , implement myself instead of using Math.pow(a,b) from library
+	 *
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double pow(int a, int b) {
+
+		double result = 1;
+
+		int powNumber = b > 0 ? b : -b;
+
+		for (int i = 0; i < powNumber; ++i) {
+			result *= a;
+		}
+
+		if (b < 0) {
+			return 1 / result;
+		}
+
+		return result;
+	}
+
+	/**
+	 * Convert String to integer, ignore any letter found, including minus sign<br>
+	 * Thus, the function always return positive integer, all minus sign in string would be discard. 
+	 * 
+	 * @param strInput
+	 * @return
+	 */
+	public int strToInt(String strInput) {
+
+		int output = 0;
+
+		for (int i = strInput.length() - 1, j = 0; i > -1; --i) {
+
+			int number = getNumber(strInput.charAt(i));
+
+			if (number == -1) {
+				continue;
+			}
+
+			output += number * pow(10, j);
+			++j;
+		}
+
+		return output;
+	}
+
+	
+
+	public static void main(String[] args) {
+
+		MyStrToInt inst = new MyStrToInt();
+
+		int testValue = inst.strToInt("a1b2cc3d45");
+		System.out.println( testValue);
+		
+		testValue = inst.strToInt("abc12345");
+		System.out.println( testValue);
+		
+		testValue = inst.strToInt("12345abc");
+		System.out.println(testValue);
+	}
+
+}
---
+++ test/MyStrToIntTest.java
@@ -0,0 +1,88 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for MyStrToIntTest class
+ * 
+ * @author nakarin_sup
+ *
+ */
+class MyStrToIntTest {
+
+	private MyStrToInt inst;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		inst = new MyStrToInt();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testStrToInt_pureDigits() {
+		assertEquals(6587421, inst.strToInt("6587421"));	
+	}
+	
+	@Test
+	void testStrToInt_pureLetters() {
+		assertEquals(0, inst.strToInt("asdilfuoewr"));	
+	}
+	
+	@Test
+	void testStrToInt_pureSpecialChars() {
+		assertEquals(0, inst.strToInt("@#!$%^&*()_+"));	
+	}
+	
+	@Test
+	void testStrToInt_mixSpecialCharsAndDigits() {
+		assertEquals(6587421, inst.strToInt("!@!@6\\/|}[p]#$%5^&8#$74%$#2!@1"));
+	}
+	
+	@Test
+	void testStrToInt_mixLettersAndSpecialCharsAndDigits() {
+		assertEquals(6587421, inst.strToInt("6ee5a8ttt74%$#2!@1"));
+	}
+	
+	
+	@Test
+	void testStrToInt_mixString_startWithLetter_endWithLetter() {
+		assertEquals(6587421, inst.strToInt("Ee6ppe5#$%wq8xcx7ere421eredf"));
+	}
+	
+	@Test
+	void testStrToInt_mixString_startWithDigit_endWithLetter() {
+		assertEquals(6587421, inst.strToInt("6ppe5eREwq8xcx7e#$421eredf"));
+	}
+	
+	@Test
+	void testStrToInt_mixString_startWithLetter_endWithDigit() {
+		assertEquals(6587421, inst.strToInt("ee6ppe5eREWq8x*(7ere421"));
+	}
+	
+	@Test
+	void testStrToInt_mixString_startWithDigit_endWithDigit() {
+		assertEquals(6587421, inst.strToInt("6pp$%5eREWq8xcx7ere42REEW1"));
+	}
+	
+	@Test
+	void testStrToInt_mixString_startWithSpecialChar_endWithSpecialChar() {
+		assertEquals(6587421, inst.strToInt("#$#6pp$%5eREWq8xcx7ere42REEW1**"));
+	}
+		
+	
+	@Test
+	void testStrToInt_mixString_startWithNotDigit_endWithDigit() {
+		assertEquals(6587421, inst.strToInt("ONETWOTREE#$#6587421"));
+	}
+	
+	@Test
+	void testStrToInt_mixString_startDigit_endWithNotDigit() {
+		assertEquals(6587421, inst.strToInt("6587421*FOUR*FIVE"));
+	}
+
+}
